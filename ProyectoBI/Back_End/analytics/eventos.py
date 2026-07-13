44e447695a20a851a9000e465d8691616fbc199d
mperezriv-c <mperezriv@ucvvirtual.edu.pe>
2026-07-13 15:51:57 -0500

    Add files via upload

---
+++ ProyectoBI/Back_End/analytics/eventos.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+import uuid
+
+
+def enviar_evento(nombre_evento):
+
+    url = (
+        "https://www.google-analytics.com/mp/collect"
+        f"?measurement_id={st.secrets['GA4_ID']}"
+        f"&api_secret={st.secrets['GA4_SECRET']}"
+    )
+
+    datos = {
+        "client_id": str(uuid.uuid4()),
+        "events": [
+            {
+                "name": nombre_evento
+            }
+        ]
+    }
+
+    respuesta = requests.post(
+        url,
+        json=datos
+    )
+
+    return respuesta.status_code
