42b0b807d99ffb35d17c999b83627f72453db9fe
mperezriv-c <mperezriv@ucvvirtual.edu.pe>
2026-07-13 16:16:12 -0500

    Update eventos.py

--- ProyectoBI/Back_End/analytics/eventos.py
+++ ProyectoBI/Back_End/analytics/eventos.py
@@ -24,5 +24,7 @@ def enviar_evento(nombre_evento):
         url,
         json=datos
     )
+    print("GA4 STATUS:", respuesta.status_code)
+    print("GA4 RESPONSE:", respuesta.text)
 
     return respuesta.status_code
