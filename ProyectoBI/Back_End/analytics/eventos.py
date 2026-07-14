852de42e8d5cc69d2ead8e3bfc08ed6649b74a67
mperezriv-c <mperezriv@ucvvirtual.edu.pe>
2026-07-13 20:40:49 -0500

    Update eventos.py

--- ProyectoBI/Back_End/analytics/eventos.py
+++ ProyectoBI/Back_End/analytics/eventos.py
@@ -5,44 +5,35 @@ from Back_End.analytics.estadisticas import registrar_evento
 
 def enviar_evento(nombre_evento):
 
-    ga4_id = st.secrets.get("GA4_ID")
-    ga4_secret = st.secrets.get("GA4_SECRET")
-
-    if not ga4_id or not ga4_secret:
-        print("ERROR: No se encontraron GA4_ID o GA4_SECRET.")
-        return None
-
-    # Mantener el mismo client_id durante toda la sesión
-    if "ga_client_id" not in st.session_state:
-        st.session_state["ga_client_id"] = str(uuid.uuid4())
-
-    client_id = st.session_state["ga_client_id"]
-
     url = (
         "https://www.google-analytics.com/mp/collect"
-        f"?measurement_id={ga4_id}"
-        f"&api_secret={ga4_secret}"
+        f"?measurement_id={st.secrets.get('GA4_ID')}"
+        f"&api_secret={st.secrets['GA4_SECRET']}"
     )
 
     datos = {
-        "client_id": client_id,
+        "client_id": str(uuid.uuid4()),
         "events": [
             {
                 "name": nombre_evento,
                 "params": {
-                    "engagement_time_msec": 100
+                    "app": "Plataforma_BI_Universitaria"
                 }
             }
         ]
     }
-    respuesta = requests.post(
-        url,
-        json=datos
-    )
+
+    respuesta = requests.post(url, json=datos)
+
 
     print("EVENTO:", nombre_evento)
     print("GA4 STATUS:", respuesta.status_code)
     print("GA4 RESPONSE:", respuesta.text)
 
-    registrar_evento(nombre_evento)
+
+    if "estadisticas" in st.session_state:
+        if nombre_evento in st.session_state.estadisticas:
+            st.session_state.estadisticas[nombre_evento] += 1
+
+
     return respuesta.status_code
